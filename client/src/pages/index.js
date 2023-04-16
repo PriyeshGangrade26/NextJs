fb087c9ee0e42d2bcf184c3f03904da1b3529670
PriyeshGangrade26 <priyeshgangrade786@gmail.com>
2023-04-16 19:55:36 +0530

    Next Js Template

---
+++ client/src/pages/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Head from "next/head";
+
+const index = () => {
+  return (
+    <>
+      <Head>
+        <title>Next Js</title>
+        <link
+          rel="shortcut icon"
+          href="/favicon.ico?"
+          type="image/x-icon"
+        />
+      </Head>
+      <div>Hello World</div>
+    </>
+  );
+};
+
+export default index;
